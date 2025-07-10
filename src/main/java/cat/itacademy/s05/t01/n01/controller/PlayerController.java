7e1e223475ade3478119730cf7ebaf750ab0ff65
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-07-10 12:41:35 +0200

    PlayerDTO updated with new User Validation class

--- src/main/java/cat/itacademy/s05/t01/n01/controller/PlayerController.java
+++ src/main/java/cat/itacademy/s05/t01/n01/controller/PlayerController.java
@@ -1,5 +1,29 @@
 package cat.itacademy.s05.t01.n01.controller;
 
+import cat.itacademy.s05.t01.n01.dto.PlayerDTO;
+import cat.itacademy.s05.t01.n01.dto.PlayerNameUpdateRequest;
+import cat.itacademy.s05.t01.n01.service.PlayerService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/player")
 public class PlayerController {
-    
+
+    private final PlayerService playerService;
+
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @PutMapping
+    public Mono<ResponseEntity<PlayerDTO>> updateById(@PathVariable Long id, @Valid @RequestBody PlayerNameUpdateRequest request) {
+
+        return playerService.updatePlayerName(id, request.name())
+                .map(ResponseEntity::ok);
+
+
+    }
 }
