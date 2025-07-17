92863537938aef334302d411d2798f8795c6b5d5
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-07-17 16:08:07 +0200

    Insert DebugController to verify swagger-config.yaml

---
+++ src/main/java/cat/itacademy/s05/t01/n01/controller/DebugController.java
@@ -0,0 +1,22 @@
+package cat.itacademy.s05.t01.n01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/debug")
+public class DebugController {
+
+    @GetMapping("/swagger-config")
+    public Mono<String> getSwaggerConfig() {
+        InputStream is = getClass().getResourceAsStream("/META-INF/resources/swagger-ui/swagger-config.yaml");
+        if (is == null) {
+            return Mono.just("❌ not founded to JAR");
+        }
+        return Mono.just("✅ swagger-config.yaml founded to JAR");
+    }
+}
