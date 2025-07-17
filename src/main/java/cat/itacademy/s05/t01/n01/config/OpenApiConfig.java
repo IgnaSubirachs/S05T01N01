f64d4313a2f206f844f881dd0c2af64794bb4b79
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-07-17 19:06:10 +0200

    Fixing Swagger and pom dependencies

--- src/main/java/cat/itacademy/s05/t01/n01/config/OpenApiConfig.java
+++ src/main/java/cat/itacademy/s05/t01/n01/config/OpenApiConfig.java
@@ -18,8 +18,8 @@ import org.springframework.context.annotation.Configuration;
         ),
         servers = {
                 @Server(
-                        url = "https://blackjackapi-ignasisubirachs.onrender.com",
-                        description = "Render Server"
+                        url = "http://localhost:8080",
+                        description = "Localhost"
                 )
         }
 )
