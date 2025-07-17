54a3f43d12818ad4cc014dddbf4d315c29a1c113
Ignasi Subirachs <ignasubirachs@gmail.com>
2025-07-17 18:43:11 +0200

    Editing mail without .env configuration

--- src/main/java/cat/itacademy/s05/t01/n01/S05T01N01Application.java
+++ src/main/java/cat/itacademy/s05/t01/n01/S05T01N01Application.java
@@ -1,21 +1,14 @@
 package cat.itacademy.s05.t01.n01;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class S05T01N01Application {
 
-	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
-
-		SpringApplication.run(S05T01N01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(S05T01N01Application.class, args);
+    }
 }
